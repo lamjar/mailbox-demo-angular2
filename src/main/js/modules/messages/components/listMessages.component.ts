a93025103586f72ad59398c3393bab8ac35f27e7
Christian Ulbrich <christian.ulbrich@zalari.de>
2016-07-14 00:07:02 +0200

    add ngFor and clickHandler for setting active message, add Message model

--- src/main/js/modules/messages/components/listMessages.component.ts
+++ src/main/js/modules/messages/components/listMessages.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { Message } from '../models/message.model';
 
 @Component({
   templateUrl: '../views/listMessages.view.html',
   selector: 'list-messages'
 })
 export class ListMessagesComponent {
-  message = {
-    from: 'me@me.com',
-    to: 'to@you.com',
-    subject: 'test',
-    time: new Date()
+  messages : Message[] = [
+    {
+      id : 1,
+      from: 'me@me.com',
+      to: 'to@you.com',
+      subject: 'test',
+      time: new Date(),
+      body: 'lorem uisdv'
+    },
+
+    {
+      id: 2,
+      from: 'me2@me.com',
+      to: 'to2@you.com',
+      subject: 'test',
+      time: new Date(),
+      body: 'jksdjvdksjvds'
+    }
+
+  ];
+
+  active = this.messages[0];
+
+  setActive(message: Message) {
+    console.log('active message is', message);
+    this.active = message;
   };
 
 
